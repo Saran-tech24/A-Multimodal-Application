fc8994eca91f9850516413483df0b25273dd9fcb
Saran-tech24 <saranbalan8@gmail.com>
2024-10-18 09:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ model_id1 = "dreamlike-art/dreamlike-diffusion-1.0"
 model_id2 = "stabilityai/stable-diffusion-xl-base-1.0"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id1, torch_dtype=torch.float16, use_safetensors=True)
-pipe = pipe.to("cuda")
+pipe = pipe.to("cpu")
 
 prompt = """dreamlikeart, a grungy woman with rainbow hair, travelling between dimensions, dynamic pose, happy, soft eyes and narrow chin,
 extreme bokeh, dainty figure, long hair straight down, torn kawaii shirt and baggy jeans
@@ -55,7 +55,7 @@ def process_audio(audio_path, image_option):
         try:
             model_id1 = "dreamlike-art/dreamlike-diffusion-1.0"
             pipe = StableDiffusionPipeline.from_pretrained(model_id1, torch_dtype=torch.float16, use_safetensors=True)
-            pipe = pipe.to("cuda")
+            pipe = pipe.to("cpu")
             image = pipe(translation).images[0]
         except Exception as e:
             return tamil_text, translation, f"An error occurred during image generation: {str(e)}"
